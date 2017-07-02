191add5051a42dc358e60c1b899df8db1c208a44
20161104589 <915327031@qq.com>
2017-07-02 18:58:58 +0800

    使用while循环写入

--- GPS/main.c
+++ GPS/main.c
@@ -16,38 +16,119 @@
 
 /*“˝»ÎÕ∑Œƒº˛*/
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 
 int main()
 {
+    char a1[70];
+    char a[70];
     char hour[10];/*定义小时*/
-    char min[10];/*定义分*/
+    char mine[10];/*定义分*/
     char sec[10];/*定义秒*/
     char lat[10];/*定义纬度和分*/
     char latfen[10];
     char nors[10];/*定义南北纬*/
     char lon[10];/*定义经度和分*/
-    char lonfen[10];
+    char lofen[10];
     char wors[10];/*定义东西经*/
-    char qua[14];/*质量因子*/
-    char sum[10];/*可使用的卫星数*/
-    char lev[10];/*水平精度因子*/
-    char ant[10];/*天线高程*/
-    char ear[10];/*大地椭圆相对海平面的高度*/
-    char yea[10];/*差分GPS数据年龄*/
-    char sta[10];/*差分基准站号*/
-    
-    FILE *fe;
-    fe=fopen("//Users//a20161104589//Desktop//G//G//G//GPS.log","r+");
-    while(scanf(fe,"%s %s %s %s %s %s %s %s %s %s %s %s %s %s",hour,min,sec,lat,nors,lon,wors,qua,sum,lev,ant,ear,yea,sta)!=EOF)
-    {
-        printf("%s %s %s %s %s %s %s %s %s %s %s %s %s %s",hour,min,sec,lat,nors,lon,wors,qua,sum,lev,ant,ear,yea,sta);
-    }
-    
-    
+    FILE *fr;
+    FILE *fw;
+    int b,c,d,e,f,g,h,i,j;
+    fr=fopen("//Users//a20161104589//Desktop//g//GPS//GPS//GPS.log","r+");
+    fw=fopen("//Users//a20161104589//Desktop//g//GPS//GPS//gps.csv","w");
+    if(fr==NULL)
+        printf("打开文件错误");
+    else
+        fprintf(fw,"\n");
+     while(!feof(fr))
+            {
+                   fscanf(fr,"%s%s",a1,a);
+                   printf("输出：%s\n%s",a1,a);
+                   for(b=0;b<2;b++)
+                   {
+                       hour[b]=a[b+7];
+                   }
+                   hour[2]='\0';
+                   printf("%s时 ",hour);
+                   fprintf(fw,"%s时 ",hour);
+                   for(c=0;c<2;c++)
+                   {
+                       mine[c]=a[c+9];
+                   }
+                   mine[2]='\0';
+                   printf("%s分 ",mine);
+                   fprintf(fw,"%s分 ",mine);
+                   for(d=0;d<2;d++)
+                   {
+                       sec[d]=a[d+11];
+                   }
+                   sec[2]='\0';
+                   printf("%s秒 ",sec);
+                   fprintf(fw,"%s秒 ",sec);
+                   for(e=0;e<2;e++)
+                   {
+                       lat[e]=a[e+14];
+                   }
+                   lat[2]='\0';
+                   printf("%s度",lat);
+                   fprintf(fw,"%s度",lat);
+                   for(f=0;f<6;f++)
+                   {
+                       latfen[f]=a[f+16];
+                   }
+                   latfen[6]='\0';
+                   printf("%s分",latfen);
+                   fprintf(fw,"%s分",latfen);
+                   for(g=0;g<1;g++)
+                   {
+                       nors[g]=a[g+23];
+                   }
+                   mine[1]='\0';
+                   if(nors[g]=='S')
+                   {
+                       printf("南纬");
+                       fprintf(fw,"南纬");
+                   }
+                   else
+                   {
+                       printf("北纬");
+                       fprintf(fw,"北纬");
+                   }
+                   for(h=0;h<3;h++)
+                   {
+                       lon[h]=a[h+25];
+                   }
+                    lon[3]='\0';
+                   printf("%s度",lon);
+                   fprintf(fw,"%s度",lon);
+                   for(i=0;i<6;i++)
+                   {
+                       lofen[i]=a[i+28];
+                   }
+                   lofen[6]='\0';
+                   printf("%s分",lofen);
+                   fprintf(fw,"%s分",lofen);
+                   for(j=0;j<1;j++)
+                   {
+                       wors[j]=a[j+35];
+                   }
+                   wors[1]='\0';
+                   if(wors[j]=='E')
+                   {
+                       printf("东");
+                       fprintf(fw,"东经\n");
+                   }
+                   else
+                   {
+                       printf("西");
+                       fprintf(fw,"西经\n");
+                   }
+
+               }
+            
+
 }
+
+
 
 
 
